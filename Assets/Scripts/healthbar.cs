4f8ad9727c395df0899d2aa649c44469c10d0c10
ccnykid <krendon27@yahoo.com>
2015-10-25 16:47:27 -0400

    Edited health script. The big cube still doesn't take damage or get destroyed.

--- Assets/Scripts/healthbar.cs
+++ Assets/Scripts/healthbar.cs
@@ -5,13 +5,26 @@ public class healthbar : MonoBehaviour {
 	
 	public float health = 100f;
 	
+	public void damageTaken(float damage)
+	{
+		health -= damage;
+	}
+	
 	void OnTriggerEnter(Collider c) {
-		if(c.CompareTag("Block")) {
-			health -= 25;
-			if(health == 0){
+		if(c.CompareTag("Block")) 
+		{
+			if (health > 0)
+			{
+				damageTaken(25f);
+				Destroy(gameObject);
+			}
+			else if (health == 0)
+			{
 				Destroy(c.gameObject);
+				Destroy(gameObject);
 			}
 		}
+		Destroy(gameObject);
 	}
 
 	// Use this for initialization
