53c04900722d27d81b4816951a443a48f12ba53c
Claudia Y <claudiab.yan@gmail.com>
2015-11-21 15:15:37 -0500

    movement particles script

--- Assets/Scripts/Misc Testing Scripts/Particles.cs
+++ Assets/Scripts/Misc Testing Scripts/Particles.cs
@@ -2,21 +2,20 @@
 using System.Collections;
 
 public class Particles : MonoBehaviour {
-
-
+    public MovementStates mstate;
     public ParticleSystem moveparticles;
     public ParticleSystem dashparticles;
-    public KeyCode dashInput = KeyCode.LeftAlt;
-    public KeyCode left = KeyCode.LeftArrow;
-    public KeyCode right = KeyCode.RightArrow;
-    public KeyCode forward = KeyCode.UpArrow;
-    public KeyCode back = KeyCode.DownArrow;
+    //public KeyCode dashInput = KeyCode.LeftAlt;
+    //public KeyCode left = KeyCode.LeftArrow;
+    //public KeyCode right = KeyCode.RightArrow;
+    //public KeyCode forward = KeyCode.UpArrow;
+    //public KeyCode back = KeyCode.DownArrow;
 
     /// <summary>
     /// 
     /// </summary>
     void Awake()
-    {
+    { 
         // particle = GetComponentsInChildren<ParticleSystem>();
     }
     void Playtheparticles()
@@ -25,16 +24,28 @@ public class Particles : MonoBehaviour {
     }
     // Use this for initialization
     void Start () {
-    
+        //if (Input.GetKeyDown(left) || Input.GetKeyDown(right) || Input.GetKeyDown(forward) || Input.GetKeyDown(back))
+       
+
     }
 
     // Update is called once per frame
     void Update ()
     {
-        if (Input.GetKeyDown(left) || Input.GetKeyDown(right) || Input.GetKeyDown(forward) || Input.GetKeyDown(back))
+        if (mstate == MovementStates.isMoving)
         {
             moveparticles.Play();
         }
+        if (mstate == MovementStates.isDashing)
+        {
+            dashparticles.Play();
+        }
+        else
+        {
+            moveparticles.Pause();
+        }
+       
+
     }
 
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -128,4 +128,5 @@ public class Movement : MonoBehaviour {
         return currentState;
     }
     #endregion
+
 }
