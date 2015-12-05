d2af8576e21e498be0a8737af05bad0e7d6a1ca1
Porrith Suong <psuong95@gmail.com>
2015-12-04 23:38:17 -0500

    Adds method fixes to the camera script.

--- Assets/Scripts/Camera Scripts/CameraControl.cs
+++ Assets/Scripts/Camera Scripts/CameraControl.cs
@@ -16,39 +16,35 @@ public class CameraControl : MonoBehaviour {
     private void Awake() {
         camera = GetComponentInChildren<Camera>();
     }
-
-
+    
     private void FixedUpdate() {
         // Move the camera towards a desired position.
         Move();
 
         // Change the size of the camera based.
-        Zoom();
+        ZoomCamera();
     }
-
-
+    
     private void Move() {
         // Find the average position of the targets.
-        FindAveragePosition();
+        GetAvgPosition();
 
         // Smoothly transition to that position.
         transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref moveVelocity, dampTime);
     }
 
-
-    private void FindAveragePosition() {
+    private void GetAvgPosition() {
         Vector3 averagePos = new Vector3();
         int numberOfTargets = 0;
 
         // Go through all the targets and add their positions together.
         for (int i = 0; i < playerTargets.Length; i++) {
             // If the target isn't active, go on to the next one.
-            if (!playerTargets[i].gameObject.activeSelf)
-                continue;
-
-            // Add to the average and increment the number of targets in the average.
-            averagePos += playerTargets[i].position;
-            numberOfTargets++;
+            if (playerTargets[i].gameObject.activeSelf) {
+                // Add to the average and increment the number of targets in the average.
+                averagePos += playerTargets[i].position;
+                numberOfTargets++;
+            }
         }
 
         // If there are targets divide the sum of the positions by the number of them to find the average.
@@ -61,59 +57,54 @@ public class CameraControl : MonoBehaviour {
         // The desired position is the average position;
         desiredPosition = averagePos;
     }
-
-
-    private void Zoom() {
+    
+    private void ZoomCamera() {
         // Find the required size based on the desired position and smoothly transition to that size.
-        float requiredSize = FindRequiredSize();
+        float requiredSize = GetSize();
         camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, requiredSize, ref zoomSpeed, dampTime);
     }
-
-
-    private float FindRequiredSize() {
+    
+    private float GetSize() {
         // Find the position the camera rig is moving towards in its local space.
         Vector3 desiredLocalPos = transform.InverseTransformPoint(desiredPosition);
 
         // Start the camera's size calculation at zero.
         float size = 0f;
 
-        // Go through all the targets...
         for (int i = 0; i < playerTargets.Length; i++) {
-            // ... and if they aren't active continue on to the next target.
-            if (!playerTargets[i].gameObject.activeSelf)
-                continue;
+            // Check if the player is active in the Hierarchy
+            if (playerTargets[i].gameObject.activeSelf) {
+                // Get the position of the target in the camera's local space
+                Vector3 targetLocalPos = transform.InverseTransformPoint(playerTargets[i].position);
 
-            // Otherwise, find the position of the target in the camera's local space.
-            Vector3 targetLocalPos = transform.InverseTransformPoint(playerTargets[i].position);
+                // Get the position of the target from the desired position of the camera's local space
+                Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
 
-            // Find the position of the target from the desired position of the camera's local space.
-            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+                // Choose the largest out of the current size and the distance of the tank 'up' or 'down' from the camera
+                size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
 
-            // Choose the largest out of the current size and the distance of the tank 'up' or 'down' from the camera.
-            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
-
-            // Choose the largest out of the current size and the calculated size based on the tank being to the left or right of the camera.
-            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / camera.aspect);
+                // Choose the largest out of the current size and the calculated size based on the tank 
+                // being to the left or right of the camera
+                size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / camera.aspect);
+            }
         }
 
-        // Add the edge buffer to the size.
+        // Adds the edge's screen buffer
         size += screenEdgeBuffer;
 
-        // Make sure the camera's size isn't below the minimum.
+        // Checks the camera's size
         size = Mathf.Max(size, minSize);
 
         return size;
     }
-
-
-    public void SetStartPositionAndSize() {
-        // Find the desired position.
-        FindAveragePosition();
+    
+    public void SetStartAndPosition() {
+        GetAvgPosition();
 
         // Set the camera's position to the desired position without damping.
         transform.position = desiredPosition;
 
         // Find and set the required size of the camera.
-        camera.orthographicSize = FindRequiredSize();
+        camera.orthographicSize = GetSize();
     }
 }
---
+++ Assets/Scripts/Misc Testing Scripts/LaunchProjectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
