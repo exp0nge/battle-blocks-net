2b801ccbe41b05eaa44acd06eef1d70149157680
Porrith Suong <psuong95@gmail.com>
2015-12-07 00:05:07 -0500

    Cleaned up prefabs and UI Screens, adds event based updates instead of running every frame.

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,33 +1,43 @@
 using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
-    public ParticleSystem deathEffect;
-    //public ParticleSystem takeHitEffect;
-    //private Vector3 particleLocation;
-    //public Transform projectile;
-	public float health = 100f;
-    //public ParticleSystem takeHit;
 
-    /// <summary>
-    /// Performs calculation on object health if health > 0
-    /// </summary>
-    /// <param name="damage"></param>
-    ///
-    void Awake() {
-        //particle = GetComponentsInChildren<ParticleSystem>();
+    public float health = 100f;
+    public ParticleSystem deathEffect;
+    public Image healthHUD;
+
+    #region Colors
+    public Color threeQuartersHealthColor;
+    public Color halfHealthColor;
+    public Color criticalHealthColor; 
+    #endregion
+
+    // Reference to the public field, health
+    private float currentHealth;
+
+    private void Awake() {
+        currentHealth = health;
     }
 
-
+    /// <summary>
+    /// Calculates the health of the current player. If currentHealth
+    /// falls below 0, then the object is destroyed. Additionally sets the
+    /// health bar to an appropriate color depending on the health.
+    /// </summary>
+    /// <param name="damage">Float value, amount to subtract</param>
     public void takeDamage(float damage)
 	{
-		health -= damage;
+		currentHealth -= damage;
+        healthHUD.fillAmount = currentHealth / 100f;
 
-        if (health <= 0)
-        {
-            Instantiate(deathEffect, transform.position , Quaternion.Euler(270,0,0)); // instantiates and plays Explode 
-            Destroy(gameObject);
-            Destroy(deathEffect);
+        if (currentHealth <= 0) {
+            Instantiate(deathEffect, transform.position, Quaternion.Euler(270, 0, 0)); // instantiates and plays Explode 
+            gameObject.SetActive(false);
         }
-	}
+        else if (currentHealth <= 66 && currentHealth > 33)
+            healthHUD.color = Color.yellow;
+        else if (currentHealth <= 33 && currentHealth > 0)
+            healthHUD.color = Color.red;
+    }
 }
