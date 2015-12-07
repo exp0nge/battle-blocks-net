02c5447009dbc45224684777dd2b5aac36fc5972
Porrith Suong <psuong95@gmail.com>
2015-12-07 01:47:46 -0500

    Move all images into a texture folder, adds takeHit particle to projectile.

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -1,26 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour {
-    //public ParticleSystem takeHit;
+
     public float damage = 20f; //Damage the projectile does.
 
-    void Awake()
-    {
-      //  takehit = GetComponentInChildren<ParticleSystem>();
+    private ParticleSystem damageParticles;
+
+    private void Awake() {
+        // damageParticles = GetComponentInChildren<ParticleSystem>();
+        // Debug.Log(damageParticles.name);
     }
 
     /// <summary>
     /// Collider detection, checks for the collider's parent object tag.
     /// </summary>
     /// <param name="other">"Other" references an outside collider that is hitting the current Object.</param>
-    void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other) {
 		if (other.CompareTag("Player")) {
 			other.GetComponent<Health>().takeDamage(damage);
-            //Instantiate(takeHit, transform.position, Quaternion.Euler(1, 180, -90));// creates takeHit particles at impact
-            Destroy(gameObject);
-            
 		}
         else if (other.CompareTag("Obstacle")) {
             Destroy(gameObject);
