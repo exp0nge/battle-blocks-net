5dfc2b6132509ba67333121d34cc0201ab037936
Porrith Suong <psuong95@gmail.com>
2015-12-09 17:19:45 -0500

    Add lifetime functionality to powerups like BurstFire, the trigger gameObject should trigger that function.

--- Assets/Scripts/Misc Testing Scripts/ActivateShooter.cs
+++ Assets/Scripts/Misc Testing Scripts/ActivateShooter.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class ActivateShooter : MonoBehaviour {
 
     // TODO: Set Coroutine to control how long the gameObject lives.
+    // TODO: Make have the power up handle it's life, this gameObject should just
+    // call the lifetime function in each power up.
     // Use polymorphism.
     // Random spawning will have to be set using a preset locations instead of something completely random,
     // this will allow for various kinds of maps.
 
-    public GameObject player;
-    public float timer = 10f;
+    public float timer = 30f;
 
 	// Use this for initialization
 	void Start () {
@@ -23,16 +24,23 @@ public class ActivateShooter : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            player.GetComponent<LaunchProjectile>().enabled = !player.GetComponent<LaunchProjectile>().enabled;
-            player.GetComponent<BurstFire>().enabled = !player.GetComponent<BurstFire>().enabled;
-            StartCoroutine(resetComponents());
+            other.GetComponent<LaunchProjectile>().enabled = !other.GetComponent<LaunchProjectile>().enabled;
+            other.GetComponent<BurstFire>().enabled = !other.GetComponent<BurstFire>().enabled;
+            StartCoroutine(resetComponents(other));
+            gameObject.SetActive(false);
         }
     }
 
-    IEnumerator resetComponents() {
+    /// <summary>
+    /// Couroutine which toggles certain components on 
+    /// the player prefab.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    IEnumerator resetComponents(Collider other) {
         yield return new WaitForSeconds(timer);
-        player.GetComponent<LaunchProjectile>().enabled = !player.GetComponent<LaunchProjectile>().enabled;
-        player.GetComponent<BurstFire>().enabled = !player.GetComponent<BurstFire>().enabled;
+        other.GetComponent<LaunchProjectile>().enabled = !other.GetComponent<LaunchProjectile>().enabled;
+        other.GetComponent<BurstFire>().enabled = !other.GetComponent<BurstFire>().enabled;
         Debug.Log("Couroutine Ended");
     }
 }
