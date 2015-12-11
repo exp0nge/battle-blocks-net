2b1c7749fa75361d84ecaa66d77965fcb4e78af6
Porrith Suong <psuong95@gmail.com>
2015-12-11 01:35:32 -0500

    Adds crude color picking system that runs on void Update().

--- Assets/Scripts/UI/ColorPicker_Screen.cs
+++ Assets/Scripts/UI/ColorPicker_Screen.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ColorPicker_Screen : UI_Screen {
 
@@ -8,15 +9,29 @@ public class ColorPicker_Screen : UI_Screen {
     public string blueSlider = "Blue Slider";
     public string imagePreview = "Image";
 
+    private float redValue;
+    private float greenValue;
+    private float blueValue;
+
     protected override void Start() {
         base.Start();
+        redValue = GetColorRatio(GetSlider(redSlider));
+        greenValue = GetColorRatio(GetSlider(greenSlider));
+        blueValue = GetColorRatio(GetSlider(blueSlider));
     }
 
 
     // Update is called once per frame
-    void Update () {
-                
-	}
+    void Update() {
+        redValue = GetColorRatio(GetSlider(redSlider));
+        greenValue = GetColorRatio(GetSlider(greenSlider));
+        blueValue = GetColorRatio(GetSlider(blueSlider));
 
+        SetImageColor(imagePreview, redValue, greenValue, blueValue, 1);
+    }
+
+    public float GetColorRatio(Slider slider) {
+        return slider.value / 255f;
+    }
 
 }
