fb11ba4c72ce2b23997b3348ad53c8a737c3df7e
Porrith Suong <psuong95@gmail.com>
2015-12-11 01:28:30 -0500

    Adds UI Getters to the UI_Screen component.

---
+++ Assets/Scripts/UI/ColorPicker_Screen.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class ColorPicker_Screen : UI_Screen {
+
+    public string redSlider = "Red Slider";
+    public string greenSlider = "Green Slider";
+    public string blueSlider = "Blue Slider";
+    public string imagePreview = "Image";
+
+    protected override void Start() {
+        base.Start();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+                
+	}
+
+
+}
