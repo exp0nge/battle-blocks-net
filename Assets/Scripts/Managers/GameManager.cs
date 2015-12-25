9bedbb90dc964b19948e8fbf192ca166c926f53d
Porrith Suong <psuong95@gmail.com>
2015-12-25 13:01:39 -0500

    Adds Round Start Message to Battlefield.unity.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform[] spawnPoints = new Transform[2];    
+    public GameObject[] players = new GameObject[2];
+    public float timer = 3f;
+    public Text roundText;
+    
+    // Use this for initialization
+    private void Start() {
+        SetUpPlayer();
+        StartCoroutine("EnablePlayer");
+    }
+    
+    /// <summary>
+    /// Moves and translates each player's position to a set position.
+    /// </summary>
+    private void SetUpPlayer() {
+        for (int i = 0; i < players.Length; i++) {
+            players[i].transform.position = spawnPoints[i].position;
+            players[i].SetActive(true);
+            players[i].GetComponent<Movement>().enabled = !players[i].GetComponent<Movement>().enabled;
+        }
+    }
+    
+    private IEnumerator EnablePlayer() {
+        yield return new WaitForSeconds(timer);
+        roundText.text = "Round Start!";
+        yield return new WaitForSeconds(1f);
+        roundText.text = "";
+        for (int i = 0; i < players.Length; i++) {
+            players[i].GetComponent<Movement>().enabled = !players[i].GetComponent<Movement>().enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/TankManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
