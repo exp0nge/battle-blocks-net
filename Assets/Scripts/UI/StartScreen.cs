dbe4d071e92523d0faae52825a2dfc08a4ca3366
Porrith Suong <psuong95@gmail.com>
2015-12-25 00:38:37 -0500

    Removes unnecessary components in StartScreen gameObject, resizes player.

---
+++ Assets/Scripts/UI/StartScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : UI_Screen {
+
+    public void StartGame(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+}
