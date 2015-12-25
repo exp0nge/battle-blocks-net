d0198be0aa5a2e9a28b89862f6c889345a29b87f
Porrith Suong <psuong95@gmail.com>
2015-12-25 11:26:54 -0500

    Adds Message Canvas to Battlefield.unity.

--- Assets/Scripts/UI/StartScreen.cs
+++ Assets/Scripts/UI/StartScreen.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class StartScreen : UI_Screen {
 
+    /// <summary>
+    /// Loads the next subsequent scene.
+    /// </summary>
+    /// <param name="level">Int value, scene number to load</param>
     public void StartGame(int level)
     {
         SceneManager.LoadScene(level);
